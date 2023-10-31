211ac83841bc5405bc77f6d29f3a6e8eeb085d0a
anhyocheol <anhyocheol@naver.com>
2023-10-31 14:11:00 +0900

    feat : marketDO, matketDAO, marketAndStoreController 기능 추가

--- hositamtam/src/main/java/controller/MarketAndStoreController.java
+++ hositamtam/src/main/java/controller/MarketAndStoreController.java
@@ -1,59 +1,65 @@
 package controller;
 
-import model.TestModel;
 
-//
-//import org.junit.runners.Parameterized.Parameters;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.runners.Parameterized.Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//import model.MarketDAO;
-//import model.MarketDO;
-/*
-mark 
- 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
- */
+import model.MarketDAO;
+
+
 @Controller
 public class MarketAndStoreController {
 
-//	private MarketDAO MarketDAO;
+	private MarketDAO marketDAO = new MarketDAO();
 	
 	public MarketAndStoreController() {
 	}
-//	
-//	public MarketAndStoreController(MarketDAO MarketDAO) {
-//		this.MarketDAO = MarketDAO;
-//	}
 	
-	@GetMapping("/views/toMarket")
-	public String Market(TestModel testModel) {
-		testModel.setControllerMsg("현재 marketController에서 화면 구현 중");
-		testModel.setNo(1);
-		testModel.setName("부평시장");
-		testModel.setType("상설장");
+	public MarketAndStoreController(MarketDAO marketDAO) {
+		this.marketDAO = marketDAO;
+	}
+	// 시장 조회
+	@GetMapping("/views/marketList")
+	public String MarketList(Model model) {
+		model.addAttribute("marketList", marketDAO.getMarketList());
 		return "market";
 	}
-
+	@GetMapping("/views/marketByCategory")	
+	public String MarketListByItem(@RequestParam String cateno, Model model) {
+		model.addAttribute("marketList", marketDAO.getMarketListByItem(cateno));
+		return "market";
+	}
+	@GetMapping("/views/marketBySearch")
+	public String MarketListBySearch(@RequestParam String keyword, Model model) {
+		model.addAttribute("marketList", marketDAO.getMarketListBySearch(keyword));
+		return "market";
+	}
+	// 점포 조회
 	@GetMapping("/views/store")
-	public String StoreList(@RequestParam String name ,TestModel testModel) {
-		testModel.setName(name);
-		testModel.setStoreName("꽃분이네");
-
+	public String StoreList(@RequestParam String name , Model model) {
+		//수정예정
+		model.addAttribute("marketList", marketDAO.getMarketListByItem(name));
 		return "store";
 	}
-
 	@GetMapping("/views/storeDetail")
-	public String StoreDetail(@RequestParam String name ,TestModel testModel) {
-		testModel.setStoreName(name);
-		
+	public String StoreDetail(@RequestParam String name , Model model) {
+		//수정예정
+		model.addAttribute("marketList", marketDAO.getMarketListByItem(name));
 		return "storeDetail";
+	}
+	@GetMapping("/views/storeUpdate")
+	public String StoreUpdate(@RequestParam String name , Model model) {
+		//수정예정
+		model.addAttribute("marketList", marketDAO.getMarketListByItem(name	));
+		return "store";
 	}
 	
 
