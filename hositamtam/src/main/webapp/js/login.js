9e15ab9ba00d70afccafafa05d2abaf6a40d0d74
wlstjd3398 <wlstjd3398@naver.com>
2023-11-02 17:37:03 +0900

    회원가입, 로그인  진행

--- hositamtam/src/main/webapp/js/login.js
+++ hositamtam/src/main/webapp/js/login.js
@@ -1,41 +1,20 @@
-function loginHandler(event) {
-    event.preventDefault();
-
-    // 사용자가 입력한 아이디와 비밀번호 값을 가져옴
+function submitHandler(event) {
     let id = document.querySelector('#id').value;
     let passwd = document.querySelector('#passwd').value;
+    let msgDiv = document.querySelector('#msg'); // 에러 메시지를 표시할 요소
 
-    // 서버에 로그인 요청을 보냄
-    fetch('/views/loginMember', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id, passwd }), // 아이디와 비밀번호를 JSON 형태로 전송
-    })
-    .then(response => {
-        if (response.status === 200) {
-            // 로그인 성공 시, 메인 페이지로 이동
-            window.location.href = '/views/main';
-        } else {
-            // 로그인 실패 시, 에러 메시지를 표시
-            document.querySelector('.login__error').textContent = '아이디 혹은 패스워드가 틀렸습니다.';
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    if (!id || !passwd) {
+        event.preventDefault();
+        msgDiv.innerHTML = '아이디와 비밀번호를 모두 입력해야 합니다.';
+        // 폼 제출을 중단하고 에러 메시지를 표시
+    } else {
+        msgDiv.innerHTML = ''; // 에러 메시지를 지웁니다.
+    }
 }
 
 function init() {
-    let login__form = document.querySelector('#login__form');
-    login__form.addEventListener('submit', loginHandler);
-    
-    let signupButton = document.querySelector('#signup-button');
-    signupButton.addEventListener('click', function() {
-        // 회원가입 페이지로 이동
-        window.location.href = '/views/join';
-    });
+    let join__form = document.querySelector('#login__form');
+    join__form.addEventListener('submit', submitHandler);
 }
 
 window.addEventListener('load', init);
