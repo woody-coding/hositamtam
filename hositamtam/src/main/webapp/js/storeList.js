b83ae3af284503b56de191b73dd3e7a49d52f7bb
sft1394 <sft1394@naver.com>
2023-11-09 18:21:40 +0900

    store 이미지 카드

--- hositamtam/src/main/webapp/js/storeList.js
+++ hositamtam/src/main/webapp/js/storeList.js
@@ -281,16 +281,21 @@ function openInfo() {
             
 
 	        let infowindow = new naver.maps.InfoWindow({	// 상세페이지로, 등록(수정)페이지로 이동하는 a태그는 해당 페이지들을 제어하는 컨트롤러로 보내기
-	            content: '<div id="'+ locations[i].sno +'" class="infoContent"><div id="'+ locations[i].sno +'" class="personalInfowindowScontent"><h4>점포명: ' + locations[i].sname + '</h4>' +
+	            content: '<div id="'+ locations[i].sno +'" class="infoContent">' + 
+	            '<div id="'+ locations[i].sno +'" class="personalInfowindowScontent">' + 
+	             '<div class="infoImgContainer">' +
+                   '<img src="/finalProject/images/' + locations[i].sphoto  + '">' +
+                   '</div>' +
+                   '<div class="두개묶고 점포형태 밑에"">' +
+	            '<h4>점포명: ' + locations[i].sname + '</h4>' +
                    '<p>취급품목: ' + locations[i].scategory + '</p>' +
                    '<p>점포형태: ' +  locations[i].stype + '</p>' +
+                   '</div>' +
                    '<div class="countContainer">' +
                    '<img src="../images/2b50.png" alt="평균별점">' + locations[i].savgrating + '(' + locations[i].sreviewcount + ')' +
                    '<i class="fa-solid fa-heart"></i>: ' +  locations[i].sfavoritecount +
                    '</div>' +
-                   '<div class="imgContainer">' +
-                   '<img src="/finalProject/images/' + locations[i].sphoto  + '">' +
-                   '</div></div>' +
+                  '</div>' +
                    '<div class="btnContainer">' +
                    '<a href="/finalProject/views/storeDetail?sno=' + locations[i].sno  + '">점포 상세</a>' +
                    '<a href="/finalProject/views/storeUpdate?sno=' +locations[i].sno  + '">점포 수정</a>' +
@@ -411,16 +416,19 @@ function storeAjaxHandler() {
 		for(let i=0; i < allStoreList.length; i++) {
 			storeContents += '<div class="mkcontainer row">' +
 				'<div id="' + allStoreList[i].sno + '" class="personalScontent">' +
-				    '<p>'+
-				    '점포명: ' + allStoreList[i].sname +
-				    '</p>' +
-				    '<p>취급품목: ' + allStoreList[i].scategory + '</p>' +
-				    '<p>점포형태: ' +  allStoreList[i].stype + '</p>' +
-				    '<img src="../images/2b50.png" alt="평균별점">' + allStoreList[i].savgrating + '(' + allStoreList[i].sreviewcount + ')' +
-				    ' <i class="fa-solid fa-heart"></i>: ' + allStoreList[i].sfavoritecount +
 				    '<div class="imgContainer">' +
 				    '<img src="/finalProject/images/' + allStoreList[i].sphoto  + '">' +
 				    '</div>'+
+				    '<div>' +
+				    '<p>'+
+				    '' + allStoreList[i].sname +
+				    '</p>' +
+				    '<p> ' + allStoreList[i].scategory + '</p>' +
+				    '</div>'+
+				    '<p>점포형태: ' +  allStoreList[i].stype + '</p>' +
+				    '<img src="../images/2b50.png" alt="평균별점">' + allStoreList[i].savgrating + '(' + allStoreList[i].sreviewcount + ')' +
+				    ' <i class="fa-solid fa-heart"></i> ' + allStoreList[i].sfavoritecount +
+				    
 				'</div>'+
 				'</div>';
 
